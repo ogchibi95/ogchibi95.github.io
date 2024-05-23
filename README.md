713cdf4a83e39fe8da55dd8266846c2826f072ba
Jan Kristian Valgijainen <54046100+ogchibi95@users.noreply.github.com>
2024-05-23 06:50:00 +0300

    Update.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# ogchibi95.github.io
-My personal, project-based portfolio, hosted at Github Pages.
+# My personal, project-based portfolio, hosted at Github Pages.
---
+++ scripts/portfolio.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+(function(){
+
+    function load(){
+
+        var screen = document.body.selectElementByID("loader");
+
+        screen.css
+    }
+}())
