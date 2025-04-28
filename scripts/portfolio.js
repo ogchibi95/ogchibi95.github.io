8a4182844eaeb2f888a02128d7b3f4121cd4a213
Jan Kristian Valgijainen <54046100+ogchibi95@users.noreply.github.com>
2025-04-28 16:49:56 +0000

    Updated content.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -3,6 +3,10 @@
 
 
 
-function Portfolio(){
-    
+function import_script(let URI){
+
+    const STAG = document.createElement('script');
+    STAG.src = URI;
+    STAG.async = false;
+    document.head.appendChild(STAG);
 }
